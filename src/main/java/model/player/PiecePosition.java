37bfd045261600a6408d9dca123b3ed86b01306f
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-10 15:30:07 +0100

    made piece comparable

--- src/main/java/model/player/PiecePosition.java
+++ src/main/java/model/player/PiecePosition.java
@@ -1,11 +1,15 @@
 package model.player;
 
-public class PiecePosition {
+public class PiecePosition implements Comparable<PiecePosition> {
     private int row;
     private int col;
     public PiecePosition(int row, int col) {
         this.row = row;
         this.col = col;
+    }
+
+    public int compareTo(PiecePosition o) {
+        return !(o.getCol() == this.getCol() && o.getRow() == this.getRow()) ? 1 : 0;
     }
 
     public int getCol() {
