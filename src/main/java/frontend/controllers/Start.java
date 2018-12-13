8123c3f1c37c139e7630466f191a9d630412b3b4
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-13 11:39:05 +0100

    added basic event handlers

--- src/main/java/frontend/controllers/Start.java
+++ src/main/java/frontend/controllers/Start.java
@@ -2,7 +2,10 @@ package frontend.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
 
 public class Start extends AbstractController {
     @FXML
@@ -17,5 +20,19 @@ public class Start extends AbstractController {
     @FXML
     public void initialize() {
         //todo add listeners
+        nick.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                if (nick.getText().trim().equals("")) {
+                    start.setDisable(true);
+                } else {
+                    start.setDisable(false);
+                }
+            }
+        });
+        start.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+
+            }
+        });
     }
 }
