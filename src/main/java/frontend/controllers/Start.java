11e33bd4feaf87f83ef332ee02685a21c016b7a5
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-13 11:16:09 +0100

    frontend stuff

--- src/main/java/frontend/controllers/Start.java
+++ src/main/java/frontend/controllers/Start.java
@@ -1,4 +1,21 @@
 package frontend.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
 public class Start extends AbstractController {
+    @FXML
+    JFXButton upload;
+
+    @FXML
+    JFXButton start;
+
+    @FXML
+    JFXTextField nick;
+
+    @FXML
+    public void initialize() {
+        //todo add listeners
+    }
 }
