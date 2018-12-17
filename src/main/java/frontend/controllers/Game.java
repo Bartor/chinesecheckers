4034afea4e6935a8dcb0d8f43925a99b1f30c49e
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-17 23:22:07 +0100

    we can move up to one piece now

--- src/main/java/frontend/controllers/Game.java
+++ src/main/java/frontend/controllers/Game.java
@@ -2,6 +2,7 @@ package frontend.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import frontend.util.BoardField;
+import frontend.util.TurnState;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -9,6 +10,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import model.exceptions.MoveNotAllowedException;
 import model.exceptions.NoSuchPlayerException;
 import model.player.PiecePosition;
 import model.player.Player;
@@ -19,6 +21,8 @@ import java.util.List;
 public class Game extends AbstractController {
     private List<BoardField> fields = new ArrayList<>();
     private List<BoardField> availableMoves = new ArrayList<>();
+    private BoardField chosen;
+    private TurnState state;
 
     @FXML
     VBox boardBox;
@@ -31,6 +35,8 @@ public class Game extends AbstractController {
 
     @FXML
     public void initialize() {
+        //todo delete this, debug purposes only
+        state = TurnState.YOUR_TURN;
         map.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 System.out.println("Loading fields");
@@ -39,13 +45,37 @@ public class Game extends AbstractController {
         });
     }
 
-    private void loadFields()  {
+    private void wipeAvailable() {
+        for (BoardField field : availableMoves) {
+            field.setDisable(true);
+            field.setSelected(false);
+        }
+        availableMoves.clear();
+    }
+
+    private void loadFields() {
         int[][] boardFields = game.getBoardMovementInterface().getBoard().getBoardFields();
         for (int i = 0; i < boardFields.length; i++) {
             HBox hbox = new HBox();
             hbox.setAlignment(Pos.CENTER);
             for (int j = 0; j < boardFields[i].length; j++) {
                 final BoardField button = new BoardField("white");
+                button.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent event) {
+                        System.out.println("handled");
+                        switch (state) {
+                            case YOUR_TURN:
+                                System.out.println("your-turn");
+                                choose(button);
+                                chosen = button;
+                                break;
+                            case PIECE_CHOSEN:
+                                System.out.println("piece-chosen");
+                                move(chosen, button);
+                                break;
+                        }
+                    }
+                });
                 button.setDisable(true);
                 if (boardFields[i][j] == 0) {
                     button.setPosition(new PiecePosition(i, j));
@@ -145,11 +175,6 @@ public class Game extends AbstractController {
                         }
                     }
                     if (!n) {
-                        button.setOnAction(new EventHandler<ActionEvent>() {
-                            public void handle(ActionEvent event) {
-                                choose(button);
-                            }
-                        });
                         fields.add(button);
                         hbox.getChildren().add(button);
                     }
@@ -170,25 +195,36 @@ public class Game extends AbstractController {
         }
     }
 
-    private void choose(BoardField field) {
-        if (game.getTurn() == thisPlayer.getId()) {
-            PiecePosition[] moves = game.getBoardMovementInterface().getMoves(field.getPiece());
-            System.out.println("Printing possible moves:");
-            for (PiecePosition move : moves) {
-                System.out.println("Move: " + move);
-                for (BoardField boardField : fields) {
-                    if (boardField.getPosition().getCol() == move.getCol() && boardField.getPosition().getRow() == move.getRow()) {
-                        availableMoves.add(boardField);
-                    } else {
-                        boardField.setDisable(true);
-                    }
-                }
-            }
-            for(BoardField boardField : availableMoves) {
-                boardField.setDisable(false);
-            }
+    private void move(BoardField origin, BoardField target) {
+        target.setStyle(origin.getStyle());
+        origin.setStyle("-fx-color: white");
+        try {
+            game.getBoardMovementInterface().makeMove(origin.getPiece(), target.getPosition());
+        } catch (MoveNotAllowedException e) {
+            showAlert(e.getMessage());
         }
+        origin.setSelected(false);
+        wipeAvailable();
+        target.setPiece(origin.getPiece());
+        origin.setPiece(null);
     }
 
-
+    private void choose(BoardField field) {
+        PiecePosition[] moves = game.getBoardMovementInterface().getMoves(field.getPiece());
+        System.out.println("Printing possible moves:");
+        for (PiecePosition move : moves) {
+            System.out.println("Move: " + move);
+            for (BoardField boardField : fields) {
+                if (boardField.getPosition().getCol() == move.getCol() && boardField.getPosition().getRow() == move.getRow()) {
+                    availableMoves.add(boardField);
+                } else {
+                    boardField.setDisable(true);
+                }
+            }
+        }
+        for (BoardField boardField : availableMoves) {
+            boardField.setDisable(false);
+        }
+        state = TurnState.PIECE_CHOSEN;
+    }
 }
---
+++ src/main/java/frontend/util/TurnState.java
@@ -0,0 +1,8 @@
+package frontend.util;
+
+public enum TurnState {
+    YOUR_TURN,
+    PIECE_CHOSEN,
+    AFTER_JUMP,
+    ENEMY_TURN
+}
